4a15ceb47ecc3cf9a6b03859bfeee5c164623f4a
anabaral <anabaral@gmail.com>
2018-01-08 17:25:46 +0900

    암호화/복호화 예제

--- encrypt/enc_dec_ex.py
+++ encrypt/enc_dec_ex.py
@@ -46,30 +46,49 @@ def make_pass():
 
 def main():
     key_filepath = "C:/key.bin"
+    
+    # NEW KEY FILE IF NONE EXISTS
     if not os.path.exists(key_filepath):
         password = make_pass()
         key = hashlib.sha256(password.encode("utf-8")).digest()
         key_file_out = open(key_filepath, 'wb')
         key_file_out.write(binascii.hexlify(key))
         key_file_out.close()
+    
+    # READ KEY FILE
     key_file_in = open(key_filepath, 'rb')
     key = binascii.unhexlify(key_file_in.read())
     key_file_in.close()
 
-    print (binascii.hexlify(bytearray(key)))
-    in_filename = 'C:/Users/Administrator/Pictures/book.jpg'
-    encrypt_file(key, in_filename, out_filename='C:/Users/Administrator/Pictures/book_encrypted')
+    # for debug
+    # print (binascii.hexlify(bytearray(key)))
+
+    # ENCRYPT A FILE
+    orig_filename = 'C:/Users/Administrator/Pictures/book.jpg'
+    encrypt_file(key, orig_filename, out_filename='C:/Users/Administrator/Pictures/book_encrypted')
     print ('Encrypte Done !')
 
-    #delete original file
+    #delete original file : SKIP
 
-    #decrypt
-    decrypt_file(key, in_filename='C:/Users/Administrator/Pictures/book_encrypted', out_filename='C:/Users/Administrator/Pictures/book_decrypted.jpg')
-    #outfile = open('original.hwp')
-    #magic = outfile.read(8)
-    #print 'Magic Number : ' + magic.encode('hex')
-    #if magic.encode('hex') == 'd0cf11e0a1b11ae1':
-    #    print 'This document is a HWP file.'
+    # DECRYPT THE FILE ENCRYPTED JUST BEFORE
+    out_filename = 'C:/Users/Administrator/Pictures/book_encrypted'
+    final_filename = 'C:/Users/Administrator/Pictures/book_decrypted.jpg'
+    decrypt_file(key, in_filename= out_filename, out_filename= final_filename)
 
+    # COMPARE
+    origfile = open(orig_filename, 'rb')
+    newfile = open(final_filename, 'rb')
+    magic_orig = origfile.read(16)
+    magic_new = newfile.read(16)
+    origfile.close()
+    newfile.close()
+
+    print(magic_orig)
+    print(magic_new)
+
+    if magic_orig == magic_new:
+        print ("SEEM TO BE GOOD : original == being_encrypt_n_decrypt")
+    else:
+        print ("FAIL")
 
 main()
